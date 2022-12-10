275d01a110c638d8b14607701deef3af8ddbf6df
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-12-10 12:22:37 +0900

    feat(aop): 어드바이스 추가

--- aop/src/test/java/hello/aop/order/AopTest.java
+++ aop/src/test/java/hello/aop/order/AopTest.java
@@ -1,14 +1,17 @@
 package hello.aop.order;
 
+import hello.aop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV3.class)
 class AopTest {
 
     @Autowired
